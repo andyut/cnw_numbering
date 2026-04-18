bac4293da0310aafe2000f9262a35793276a6b16
it-support@indoguna.id <it-support@indoguna.id>
2026-04-19 06:00:08 +0700

    update

--- cnw_numbering/__manifest__.py
+++ cnw_numbering/__manifest__.py
@@ -9,11 +9,9 @@
         Indoguna-Doc Numbering
     """,
 
-    'author': "Indoguna Utama,Andy Utomo",
-    'website': "http://www.indoguna.id",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    'author': "Andy Utomo",
+    'website': "http://andyut.blogspot.com",
+ 
     # for the full list
     'category': 'others',
     'version': '0.1',
@@ -25,8 +23,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'security/group_users.xml',
-        'views/wizard_view.xml',
+        'security/group_users.xml', 
         'views/views.xml',
         'menu/menu.xml',
         'views/templates.xml',
